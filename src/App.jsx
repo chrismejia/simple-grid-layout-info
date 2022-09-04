6e69ca5cbbf23c6dfb7c3a1d47a8e2bc6ecebda7
Christian Mejia <chrismejia10@gmail.com>
2022-09-04 05:23:31 -0400

    refactor: remove react-helmet; integrate meta tags directly; reduce og-image height to 540

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import ContainerPartOne from "./components/ContainerPartOne";
 import ContainerPartTwo from "./components/ContainerPartTwo";
-import Metadata from "./components/helpers/Metadata";
 import HighlightedElement from "./components/helpers/HighlightedElement";
 import TheGrid from "./components/TheGrid";
 import OpenGraphLayout from "./components/helpers/OpenGraphLayout";
@@ -8,7 +7,6 @@ import OpenGraphLayout from "./components/helpers/OpenGraphLayout";
 function App() {
   return (
     <main className="App">
-      <Metadata />
       <ContainerPartOne />
       <HighlightedElement elementLabel={`<section className="container"/>`}>
         <div className="ex-container">
