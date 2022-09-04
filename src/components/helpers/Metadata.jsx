d0ed45df00b1f3ee6ce7aea1efe87d739ae7b915
Christian Mejia <chrismejia10@gmail.com>
2022-09-04 04:47:27 -0400

    fix: add base HTML meta description; add twitter desc, image

--- src/components/helpers/Metadata.jsx
+++ src/components/helpers/Metadata.jsx
@@ -11,11 +11,16 @@ export default function Metadata() {
         key="og:description"
         content="Flexbox wishes it could do all this as easily..."
       />
-      <meta property="og:image" content={`../og-image.png`} />
+      <meta property="og:image" content="../og-image.png" />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="https://christianmejia.dev" />
       <meta name="twitter:creator" content="Christian Mejia" />
+      <meta
+        name="twitter:description"
+        content="Flexbox wishes it could do all this as easily..."
+      />
+      <meta property="twitter:image" content="../og-image.png" />
     </Helmet>
   );
 }
