545a86114875ec71361df163523e79031389cb2b
Christian Mejia <chrismejia10@gmail.com>
2022-09-04 04:52:12 -0400

    fix: test hardcoded public og:image URL in metadata

--- src/components/helpers/Metadata.jsx
+++ src/components/helpers/Metadata.jsx
@@ -11,7 +11,10 @@ export default function Metadata() {
         key="og:description"
         content="Flexbox wishes it could do all this as easily..."
       />
-      <meta property="og:image" content="../og-image.png" />
+      <meta
+        property="og:image"
+        content="https://build-css-grid-layouts.netlify.app/og-image.png"
+      />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="https://christianmejia.dev" />
@@ -20,7 +23,10 @@ export default function Metadata() {
         name="twitter:description"
         content="Flexbox wishes it could do all this as easily..."
       />
-      <meta property="twitter:image" content="../og-image.png" />
+      <meta
+        property="twitter:image"
+        content="https://build-css-grid-layouts.netlify.app/og-image.png"
+      />
     </Helmet>
   );
 }
