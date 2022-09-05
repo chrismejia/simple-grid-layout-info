6939a343ac128eb49cfa16659bdc21f5a993feb7
Christian Mejia <chrismejia10@gmail.com>
2022-09-05 08:09:52 -0400

    style: wrap Pyramid in a fieldset

--- src/components/challenges/02-Pyramid.jsx
+++ src/components/challenges/02-Pyramid.jsx
@@ -2,12 +2,13 @@ import "./challenges.css";
 
 export default function Pyramid() {
   return (
-    <article className="grid pyramid challenge">
+    <fieldset className="grid pyramid challenge">
+      <legend style={{ padding: ".5rem 0" }}>Pyramid.jsx</legend>
       <div className="width">5 WIDE</div>
       <div className="height">3 TALL</div>
       <div className="tip">tip</div>
       <div className="middle">middle</div>
       <div className="base">base</div>
-    </article>
+    </fieldset>
   );
 }
