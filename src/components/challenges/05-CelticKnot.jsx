df377fdbc336a0a9f248177ae4154c934e5b6114
Christian Mejia <chrismejia10@gmail.com>
2022-09-05 21:14:44 -0400

    WIP: change CelticKnot to 7x7; integrate better spans

--- src/components/challenges/05-CelticKnot.jsx
+++ src/components/challenges/05-CelticKnot.jsx
@@ -1,92 +1,90 @@
 export default function CelticKnot() {
   return (
     <section className="container">
+      <img src={""} alt="" />
+
       <fieldset className="grid challenge celtic-knot">
         <legend>CelticKnot.jsx</legend>
         {/* row 1 */}
-        <div className="filled tl-curve"></div>
-        <div className="filled horiz-segment"></div>
-        <div className="filled tr-curve"></div>
-        <div />
-        <div />
-        <div />
-        <div className="filled tl-curve"></div>
-        <div className="filled horiz-segment"></div>
-        <div className="filled"></div>
-
+        <div className="filled tl-curve" />
+        <div className="filled" />
+        <div className="filled tr-curve" />
+        <div className="h-blank" />
+        <div className="filled tl-curve" />
+        <div className="filled" />
+        <div className="filled tr-curve" />
         {/* row 2 */}
-        <div className="filled vert-segment"></div>
+        <div className="filled" />
         <div />
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-
+        <div className="filled v-top-inter" />
+        <div className="filled v-overlap" />
+        <div />
+        <div className="filled" />
         {/* row 3 */}
-        <div className="filled bl-curve"></div>
-        <div className="filled h-overlap"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-
+        <div className="filled bl-curve" />
+        <div className="filled h-overlap" />
+        <div className="filled" />
+        <div className="filled" />
         {/* row 4 */}
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
-        <div className="filled"></div>
+        {/* <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/> */}
+        {/* row 5 */}
+        {/* <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/> */}
+        {/* row 6 */}
+        {/* <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/> */}
+        {/* row 7 */}
+        {/* <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/> */}
+        {/* row 8 */}
+        {/* <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/> */}
+        {/* row 9 */}
+        {/* <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/>
+        <div className="filled"/> */}
       </fieldset>
     </section>
   );
