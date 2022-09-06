76e2ded8aeb035d3d85cc27a5b7b650f4c2cbdc5
Christian Mejia <chrismejia10@gmail.com>
2022-09-06 06:05:31 -0400

    feat: complete CelticKnot; version w/ shadows and rounded corners

--- src/components/challenges/05-CelticKnot.jsx
+++ src/components/challenges/05-CelticKnot.jsx
@@ -1,7 +1,7 @@
 export default function CelticKnot() {
   return (
     <section className="container">
-      <img src={""} alt="" />
+      <img src={"../celtic-knot.jpg"} alt="" height={200} />
 
       <fieldset className="grid challenge celtic-knot">
         <legend>CelticKnot.jsx</legend>
@@ -13,78 +13,48 @@ export default function CelticKnot() {
         <div className="filled tl-curve" />
         <div className="filled" />
         <div className="filled tr-curve" />
+
         {/* row 2 */}
         <div className="filled" />
         <div />
-        <div className="filled v-top-inter" />
-        <div className="filled v-overlap" />
+        <div className="filled t-to-black" />
+        <div className="filled v-overlap t-to-black" />
         <div />
         <div className="filled" />
+
         {/* row 3 */}
         <div className="filled bl-curve" />
-        <div className="filled h-overlap" />
-        <div className="filled" />
-        <div className="filled" />
+        <div className="filled h-overlap l-to-black" />
+        <div className="filled r-to-black" />
+        <div className="filled br-curve" />
+
         {/* row 4 */}
-        {/* <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/> */}
+        <div className="v-blank" />
+        <div className="v-overlap b-to-black" />
+        <div />
+        <div className="v-blank" />
+
         {/* row 5 */}
-        {/* <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/> */}
+        <div className="filled tl-curve" />
+        <div className="filled l-to-black" />
+        <div className="filled h-overlap r-to-black" />
+        <div className="filled tr-curve" />
+
         {/* row 6 */}
-        {/* <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/> */}
+        <div className="filled" />
+        <div />
+        <div className="h-blank" />
+        <div className="filled b-to-black" />
+        <div />
+        <div className="filled" />
+
         {/* row 7 */}
-        {/* <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/> */}
-        {/* row 8 */}
-        {/* <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/> */}
-        {/* row 9 */}
-        {/* <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/>
-        <div className="filled"/> */}
+        <div className="filled bl-curve" />
+        <div className="filled" />
+        <div className="filled br-curve" />
+        <div className="filled bl-curve" />
+        <div className="filled" />
+        <div className="filled br-curve" />
       </fieldset>
     </section>
   );
