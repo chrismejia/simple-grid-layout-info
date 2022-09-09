84ae05d9750401eaec534d1546af84f6a59ff3a7
Christian Mejia <chrismejia10@gmail.com>
2022-09-09 17:05:29 -0400

    fix: add min label to 32px margins in BreakpointVisual

--- src/components/examples/BreakpointMarginsVisual.jsx
+++ src/components/examples/BreakpointMarginsVisual.jsx
@@ -9,28 +9,46 @@ export default function BreakpointMarginsVisual() {
       </article>
 
       <article className="grid container-margin-ex">
-        <div className="ex-padded filled">32px</div>
+        <div className="ex-padded filled">
+          <span>min</span>
+          <span>32px</span>
+        </div>
         <div className="ex-padded filled">
           <span>Desktop</span>
           <span>960px</span>
         </div>
-        <div className="ex-padded filled">32px</div>
+        <div className="ex-padded filled">
+          <span>min</span>
+          <span>32px</span>
+        </div>
       </article>
       <article className="grid container-margin-ex">
-        <div className="ex-padded filled">32px</div>
+        <div className="ex-padded filled">
+          <span>min</span>
+          <span>32px</span>
+        </div>
         <div className="ex-padded filled">
           <span>Widescreen</span>
           <span>1152px</span>
         </div>
-        <div className="ex-padded filled">32px</div>
+        <div className="ex-padded filled">
+          <span>min</span>
+          <span>32px</span>
+        </div>
       </article>
       <article className="grid container-margin-ex">
-        <div className="ex-padded filled">32px</div>
+        <div className="ex-padded filled">
+          <span>min</span>
+          <span>32px</span>
+        </div>
         <div className="ex-padded filled">
           <span>FullHD</span>
           <span>1344px</span>
         </div>
-        <div className="ex-padded filled">32px</div>
+        <div className="ex-padded filled">
+          <span>min</span>
+          <span>32px</span>
+        </div>
       </article>
     </div>
   );
