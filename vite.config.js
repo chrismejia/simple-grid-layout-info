84e1d705fa5778c2819fa6b8482279d19b653ae6
Christian Mejia <chrismejia10@gmail.com>
2022-09-09 16:14:07 -0400

    chore: remove vite-plugin-purgecss

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePluginFonts } from "vite-plugin-fonts";
-import htmlPurge from "vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    htmlPurge({
-      css: [
-        "src/styles/challenges.css",
-        "src/styles/container.css",
-        "src/styles/examples.css",
-        "src/styles/grid-system.css",
-        "src/styles/helpers.css",
-        "src/styles/index.css",
-      ],
-    }),
     VitePluginFonts({
       google: {
         families: [
