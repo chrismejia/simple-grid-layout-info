2dd1cbdb506cd5b6d1bb4a748321fb5dbb59d6c7
Christian Mejia <chrismejia10@gmail.com>
2022-09-13 03:07:55 -0400

    fix: remove leading slash from non-index routes in getPageNavData

--- src/utils/getPageNavData.js
+++ src/utils/getPageNavData.js
@@ -4,6 +4,8 @@ export default function getPageNavData(path) {
   if (path === "/") {
     return pageNavFlow.base;
   } else {
-    return pageNavFlow[path];
+    // Path names come in with leading slash; remove it, then use as key value
+    const noSlashPath = path.slice(1);
+    return pageNavFlow[noSlashPath];
   }
 }
