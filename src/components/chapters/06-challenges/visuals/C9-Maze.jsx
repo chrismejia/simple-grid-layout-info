92bbd95686b64d061d5982be3c10b5fd160df604
Christian Mejia <chrismejia10@gmail.com>
2022-09-30 19:20:39 -0400

    WIP: fix maze map again; add first 25 walls

--- src/components/chapters/06-challenges/visuals/C9-Maze.jsx
+++ src/components/chapters/06-challenges/visuals/C9-Maze.jsx
@@ -10,12 +10,62 @@ export default function Maze() {
       <div className="left-wall"></div>
       <div className="right-wall"></div>
       <div className="bottom-wall"></div>
-
       {/* Finish Block */}
       <div className="fin-top"></div>
       <div className="fin-left"></div>
       <div className="fin-right"></div>
       <div className="fin-b-right"></div>
+      {/*  VerticalMaze Walls */}
+      <div className="w-1" />
+      <div className="w-2" />
+      <div className="w-3" />
+      <div className="w-4" />
+      <div className="w-5" />
+      <div className="w-6" />
+      <div className="w-7" />
+      <div className="w-8" />
+      <div className="w-9" />
+      <div className="w-10" />
+      <div className="w-11" />
+      <div className="w-12" />
+      <div className="w-13" />
+      <div className="w-14" />
+      <div className="w-15" />
+      <div className="w-16" />
+      <div className="w-17" />
+      <div className="w-18" />
+      <div className="w-19" />
+      <div className="w-20" />
+      <div className="w-21" />
+      <div className="w-22" />
+      <div className="w-23" />
+      <div className="w-24" />
+      <div className="w-25" />
+      <div className="w-26" />
+      <div className="w-27" />
+      <div className="w-28" />
+      <div className="w-29" />
+      <div className="w-30" />
+      {/* <div className="w-31" />
+      <div className="w-32" />
+      <div className="w-33" />
+      <div className="w-34" />
+      <div className="w-35" /> */}
+      {/* <div className="w-36" />
+      <div className="w-37" />
+      <div className="w-38" />
+      <div className="w-39" />
+      <div className="w-40" /> */}
+      {/* <div className="w-41" />
+      <div className="w-42" />
+      <div className="w-43" />
+      <div className="w-44" />
+      <div className="w-45" /> */}
+      {/* <div className="w-46" />
+      <div className="w-47" />
+      <div className="w-48" />
+      <div className="w-49" />
+      <div className="w-50" /> */}
     </article>
   );
 }
